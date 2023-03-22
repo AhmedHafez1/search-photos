c22026b1615ceca11653f9d5b6b9418a3e19c31f
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2023-03-22 06:46:03 +0200

    Added search api

---
+++ src/api.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const searchPhotos = async (searchTerm) => {
+  const response = await axios.get('https://api.unsplash.com/photos', {
+    headers: {
+      Authorization: 'Client-ID asa8hIRihci7NqTtsdlDID1HiyrF2rEyDzretZRNbBE',
+    },
+    params: { query: searchTerm },
+  });
+
+  return response.data.results;
+};
+
+export default searchPhotos;
