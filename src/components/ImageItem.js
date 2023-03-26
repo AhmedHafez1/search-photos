a8cc8a7bd34904b0dc65ce9a94eb22dafb28d099
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2023-03-26 11:23:45 +0200

    Showing the images

--- src/components/ImageItem.js
+++ src/components/ImageItem.js
@@ -1,5 +1,9 @@
 function ImageItem({ image }) {
-  return <div>{image.id}</div>;
+  return (
+    <div>
+      <img alt={image.alt_description} src={image.urls.small}></img>
+    </div>
+  );
 }
 
 export default ImageItem;
